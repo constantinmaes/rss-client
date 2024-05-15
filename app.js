dc9b42cb1ba4ddd7f4e1e59b8ed82b157f4fd852
Constantin Maes <constantinmaes@gmail.com>
2024-05-15 19:26:12 +0200

    feat: Add scheduled job to fetch and save news from RSS feeds

--- app.js
+++ app.js
@@ -11,17 +11,31 @@ let myDatabase;
 
 const cron = require('node-cron');
 
-cron.schedule('* * * * *', () => {
+cron.schedule('* * * * *', async () => {
     console.log('Scheduled job');
     // Tableau de liens RSS
-    const links = ['...'];
-    links.forEach(async (url) => {
+    const feeds = await myDatabase.all('SELECT * FROM feeds');
+    feeds.forEach(async (feed) => {
         try {
-            const { data } = await axios.get(url);
+            const { data } = await axios.get(feed.link);
             const obj = await xml2js.parseStringPromise(data);
             const title = obj.rss.channel[0].title[0];
             const news = obj.rss.channel[0].item;
             // Sauvegarder les news dans la base de données
+            news.forEach((newsItem) => {
+                myDatabase
+                    .run(
+                        'INSERT INTO items (title, link, description, pubDate, feedId) VALUES (?, ?, ?, ?, ?)',
+                        newsItem.title[0],
+                        newsItem.link[0],
+                        newsItem.description[0],
+                        newsItem.pubDate[0],
+                        feed.id
+                    )
+                    .catch((err) => {
+                        console.error('Error inserting news', err);
+                    });
+            });
         } catch (err) {
             console.error('Something happened', err);
         }
@@ -36,7 +50,10 @@ openDb({
         myDatabase = db;
         console.log('Database opened');
         myDatabase.run(
-            'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, link TEXT, description TEXT, pubDate TEXT)'
+            'CREATE TABLE IF NOT EXISTS feeds (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, link TEXT, UNIQUE (link))'
+        );
+        myDatabase.run(
+            'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, link TEXT, description TEXT, pubDate TEXT, feedId INTEGER NOT NULL, UNIQUE (link), FOREIGN KEY (feedId) REFERENCES feeds(id) ON DELETE CASCADE)'
         );
     })
     .catch((err) => {
@@ -64,23 +81,31 @@ app.use('/users', usersRouter);
 // RSS function
 app.get('/rss', async (req, res) => {
     try {
-        const url = req.query.url; // localhost:3000/rss?url=https://www.reddit.com/.rss
-        if (!url) {
-            return res.status(400).send('No URL provided');
-        }
-        console.log('IN RSS ROUTE');
-        const { data } = await axios.get(url);
-        // const axiosResponse = await axios.get(url);
-        // const data = axiosResponse.data;
-        // console.log(data); // data contient le XML brut
-        const obj = await xml2js.parseStringPromise(data);
-        // console.log(require('util').inspect(obj, { depth: null }));
-        const title = obj.rss.channel[0].title[0];
-        const news = obj.rss.channel[0].item;
-        //return res.json(news);
+        const title = 'test';
+        const news = await myDatabase.all('SELECT * FROM items');
         res.render('rss', { news, title });
     } catch (err) {
         console.error('Something happened', err);
+    }
+});
+
+// Add feed
+app.get('/add-feed', (req, res) => {
+    res.render('add-feed');
+});
+
+app.post('/add-feed', async (req, res) => {
+    const { title, link } = req.body;
+    try {
+        await myDatabase.run(
+            'INSERT INTO feeds (title, link) VALUES (?, ?)',
+            title,
+            link
+        );
+        res.redirect('/');
+    } catch (err) {
+        console.error('Error inserting feed', err);
+        res.redirect('/add-feed');
     }
 });
 
